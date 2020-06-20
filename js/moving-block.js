826789c87318b016f42f93c9032f969d471eac0f
Huyen059 <samihuyen059@gmail.com>
2020-06-20 22:22:53 +0200

    remove the moving of enemies due to errors upon running, make play move using arrow keys

--- js/moving-block.js
+++ js/moving-block.js
@@ -1,34 +1,84 @@
+
+
 document.querySelector('#play').addEventListener('click', () => {
     ////----DEFINE VARIABLES----////
     const player = document.querySelector('.player');
     const enemies = document.querySelectorAll('.enemy');
     const field = document.querySelector('.field');
-    let blockSize = 48;
+
+    let blockSize = (field.offsetWidth < 500) ? 25 : 50;
     let randPosAll = []; //store the position of the enemies
 
     //check size of field
     console.log('offset values of field: left, top, width, and height')
-    console.log(field.offsetLeft); //position from the left of body in px
-    console.log(field.offsetTop); //position from the top of body in px
-    console.log(field.offsetWidth); //width of element in px
-    console.log(field.offsetHeight); //height of element in px
+    console.log('from left', field.offsetLeft); //position from the left of body in px
+    console.log('from top', field.offsetTop); //position from the top of body in px
+    console.log('width', field.offsetWidth); //width of element in px with padding and border
+    console.log('height', field.offsetHeight); //height of element in px with padding and border
 
     ////----DEFINE FUNCTIONS----////
-    const showPlayer = () => {
-        player.style.cssText = `
+    const setBlock = (nameOfBlock, left, top) => {
+        nameOfBlock.style.cssText = `
             width: ${blockSize}px;
             height: ${blockSize}px;
             display: block;
-            left: 0;
-            top: 0;
+            left: ${left}px;
+            top: ${top}px;
             `;
+    }
+
+    const showPlayer = () => {
+        setBlock(player, 0, 0);
     };
 
+    const movePlayer = () => {
+        document.addEventListener('keydown', (e) => {
+            let posX = player.offsetLeft;
+            let posY = player.offsetTop;
+            let maxX = field.offsetWidth - blockSize;
+            let maxY = field.offsetHeight - blockSize;
+
+            switch (e.key) {
+                case 'ArrowDown':
+                    if (posY < maxY) {
+                        posY+=5;
+                        setBlock(player, posX, posY);
+                    }
+                    break;
+                case 'ArrowUp':
+                    if (posY > 0) {
+                        posY-=5;
+                        setBlock(player, posX, posY);
+                    }
+                    break;
+                case 'ArrowRight':
+                    if (posX < maxX) {
+                        posX+=5;
+                        setBlock(player, posX, posY);
+                    }
+                    break;
+                case 'ArrowLeft':
+                    if (posX > 0) {
+                        posX-=5;
+                        setBlock(player, posX, posY);
+                    }
+                    break;
+            }
+        })
+    }
 
     //Function to generate position, take in x or y as parameter, return a number as position
     const randPosXY = (coordinate) => {
-        let randX = blockSize + 1 + Math.floor(Math.random() * (field.offsetWidth - blockSize * 2));
-        let randY = blockSize + 1 + Math.floor(Math.random() * (field.offsetHeight - blockSize * 2));
+        //range where enemies can appear: inside div, not overlap player
+        let maxPosX = field.offsetWidth - blockSize;
+        let maxPosY = field.offsetHeight - blockSize;
+        let minPosX = blockSize;
+        let minPosY = blockSize;
+        let rangeX = maxPosX - minPosX;
+        let rangeY = maxPosY - minPosY;
+
+        let randX = blockSize + Math.floor(Math.random() * rangeX);
+        let randY = blockSize + Math.floor(Math.random() * rangeY);
         return (coordinate === 'x') ? randX : randY;
     }
 
@@ -44,92 +94,14 @@ document.querySelector('#play').addEventListener('click', () => {
             //console.log('Iteration i = ' + i + ', value x: ' + randPosAll[2*i] + ', value y: ' + randPosAll[2*i+1]);
 
             //add styling for each enemy
-            enemies[i].style.cssText = `
-            width: ${blockSize}px;
-            height: ${blockSize}px;
-            display: block;
-            left: ${randPosAll[2 * i]}px;
-            top: ${randPosAll[2 * i + 1]}px;
-            `;
+            setBlock(enemies[i], randPosAll[2 * i], randPosAll[2 * i + 1]);
         }
     };
 
-    const moveEnemies = () => {
-        let increment = [1, -1];
-        console.log(randPosAll);
-        let maxPosX = field.offsetWidth - blockSize;
-        let maxPosY = field.offsetHeight - blockSize;
-        console.log('maxPosX and maxPosY:', maxPosX, maxPosY)
-
-        enemies.forEach((enemy, i) => {
-            // let moveX = increment[Math.round(Math.random())];
-            // let moveY = increment[Math.round(Math.random())];
-            let moveX, moveY;
-            const generateIncrement = () => {
-                let posX = randPosAll[2 * i];
-                let posY = randPosAll[2 * i + 1];
-                if (posX === 0){
-                    moveX = 1;
-                    moveY = increment[Math.round(Math.random())];
-                }
-
-                if (posY === 0){
-                    moveX = increment[Math.round(Math.random())];
-                    moveY = 1;
-                }
-
-                if (posX === maxPosX) {
-                    moveX = -1;
-                    moveY = increment[Math.round(Math.random())];
-                }
-
-                if (posY === maxPosY){
-                    moveX = increment[Math.round(Math.random())];
-                    moveY = -1;
-                }
-                    moveX = increment[Math.round(Math.random())];
-                    moveY = increment[Math.round(Math.random())];
-            }
-
-            generateIncrement();
-
-            const move = () => {
-
-                let currentPosX = randPosAll[2 * i];
-                let currentPosY = randPosAll[2 * i + 1];
-                console.log(currentPosX, currentPosY);
-
-                currentPosX += moveX;
-                currentPosY += moveY;
-                randPosAll[2 * i] = currentPosX;
-                randPosAll[2 * i + 1] = currentPosY;
-                enemy.style.cssText = `
-                        width: ${blockSize}px;
-                        height: ${blockSize}px;
-                        display: block;
-                        left: ${randPosAll[2 * i]}px;
-                        top: ${randPosAll[2 * i + 1]}px;
-                        `;
-
-                if (currentPosX > 0 && currentPosX < maxPosX && currentPosY > 0 && currentPosY < maxPosY) {
-                    setTimeout(move, 100);
-                } else {
-                    generateIncrement();
-                    setTimeout(move, 100);
-                }
-
-            }
-
-            move();
-
-        })
-
-        console.log(randPosAll);
-
-    }
 
     ////----RUN THE FUNCTIONS----////
     showPlayer();
+    movePlayer();
     showEnemies();
-    moveEnemies();
+
 })
